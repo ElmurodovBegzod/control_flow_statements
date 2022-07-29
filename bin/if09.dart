bd50fe0b936ad6a3d2b6dbafe169df2e28e089c0
Begzod <ebegzod477@gmail.com>
2022-07-29 14:23:25 +0500

    code09

--- bin/if09.dart
+++ bin/if09.dart
@@ -10,4 +10,18 @@
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False. */
 
-void main() {}
+bool func(int a) {
+  int b = a;
+  int a1 = a % 10;
+  a = a ~/ 10;
+  int a2 = a % 10;
+  int r = a1 * 10 + a2;
+  if (b < r) {
+    return false;
+  }
+  return true;
+}
+
+void main() {
+  print(func(21));
+}
